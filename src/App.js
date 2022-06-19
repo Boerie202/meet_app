969383d83387cf66b4492b8b98de4df4ccab8bdc
Boerie202 <93001485+Boerie202@users.noreply.github.com>
2022-06-19 18:59:46 +0100

    Updated components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import CitySearch from './CitySearch';
 import EventList from './EventList';
+import NumberOfEvents from './NumberOfEvents';
 import { getEvents, extractLocations } from './api';
 import './nprogress.css';
 
@@ -9,20 +10,27 @@ class App extends Component {
   state = {
     events: [],
     locations: [],
+    NumberOfEvents: 32,
+    currentLocation: 'all',
   }
 
 
 
-  updateEvents = (location) => {
+
+  updateEvents = (location, eventCount) => {
     getEvents().then((events) => {
-      const locationEvents = (location === 'all') ?
-        events :
-        events.filter((event) => event.location === location);
-      this.setState({
-        events: locationEvents
-      });
+      const locationEvents =
+        location === "all"
+          ? events
+          : events.filter((event) => event.location === location);
+      if (this.mounted) {
+        this.setState({
+          events: locationEvents.slice(0, this.state.numberOfEvents),
+          currentLocation: location,
+        });
+      }
     });
-  }
+  };
 
   componentDidMount() {
     this.mounted = true;
@@ -36,11 +44,28 @@ class App extends Component {
   componentWillUnmount() {
     this.mounted = false;
   }
+
+
+  updateNumberOfEvents = (numberOfEvents) => {
+    this.setState(
+      {
+        numberOfEvents,
+      },
+      this.updateEvents(this.state.locations, numberOfEvents)
+    );
+  };
+
+
+
   render() {
     return (
       <div className="App" >
-        <EventList events={this.state.events} />
+
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
+        <NumberOfEvents numberOfEvents={NumberOfEvents}
+          updateNumberOfEvents={this.updateNumberOfEvents} />
+        <EventList events={this.state.events} />
+
 
       </div>
     );
