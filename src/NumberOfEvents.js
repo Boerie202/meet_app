74967fd829a05b3b6ebd21faa0a984d49bcaa86a
Boerie202 <93001485+Boerie202@users.noreply.github.com>
2022-06-19 21:03:50 +0100

    Revert "Updates"
    This reverts commit f29de8ecfe5610dcddac9a3f3d3554c38b1db086.

---
+++ src/NumberOfEvents.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
+
+class NumberOfEvents extends Component {
+
+
+    state = {
+        numberOfEvents: 32,
+        errorText: ''
+    }
+
+    handleInputChanged = (event) => {
+        const value = event.target.value;
+        if (value < 1 || value > 32) {
+            this.setState({
+                numberOfEvents: '',
+                errorText: 'Please enter a number between 1 and 32',
+            })
+        } else {
+            this.setState({
+                numberOfEvents: value
+            });
+        }
+        this.props.updateNumberOfEvents(event.target.value);
+    };
+
+    render() {
+
+        return (
+            <div className='numberOfEvents'>
+                <div className='numberOfEventsAlert'>
+                    <ErrorAlert text={this.state.errorText} />
+                </div>
+                <input
+                    type='number'
+                    className='inputNumberOfEvents'
+                    onChange={this.handleInputChanged}
+                    value={this.state.numberOfEvents}
+                />
+            </div>
+        )
+    }
+}
+
+export default NumberOfEvents;
