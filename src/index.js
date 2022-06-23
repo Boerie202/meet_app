243dddffe48e04140ab71c68dca15539bc081368
Boerie202 <93001485+Boerie202@users.noreply.github.com>
2022-06-23 21:58:29 +0100

    .

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './service-worker';
+// import reportWebVitals from './reportWebVitals';
+// import * as serviceWorker from './service-worker';
 import * as atatus from 'atatus-spa';
 
 
@@ -18,7 +18,8 @@ ReactDOM.render(
 // // If you want your app to work offline and load faster, you can change
 // // unregister() to register() below. Note this comes with some pitfalls.
 // // Learn more about service workers: https://cra.link/PWA
-serviceWorker.registerRoute();
+// serviceWorker.register();
+serviceWorkerRegistration.register();
 
 // // If you want to start measuring performance in your app, pass a function
 // // to log results (for example: reportWebVitals(console.log))
