3069bf5ff818bdadf8080f0a5528db1fd9f1e874
Boerie202 <93001485+Boerie202@users.noreply.github.com>
2022-06-26 14:32:15 +0100

    Code updates

--- src/App.js
+++ src/App.js
@@ -1,53 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
-import CitySearch from './CitySearch';
 import EventList from './EventList';
+import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
 import './nprogress.css';
+import { OfflineAlert } from './Alert';
 import WelcomeScreen from './WelcomeScreen';
-
+import EventGenre from './EventGenre';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 class App extends Component {
+
   state = {
     events: [],
     locations: [],
-    NumberOfEvents: 32,
-    currentLocation: 'all',
+    locationSelected: 'all',
+    numberOfEvents: 10,
+    OfflineAlertText: "",
     showWelcomeScreen: undefined
   }
-
-
-
-
-  updateEvents = (location, eventCount) => {
-    getEvents().then((events) => {
-      const locationEvents =
-        location === "all"
-          ? events
-          : events.filter((event) => event.location === location);
-      if (this.mounted) {
-        this.setState({
-          events: locationEvents.slice(0, this.state.numberOfEvents),
-          currentLocation: location,
-        });
-      }
-    });
-  };
-
 
   async componentDidMount() {
     this.mounted = true;
     const accessToken = localStorage.getItem('access_token');
-    const isTokenValid = (await checkToken(accessToken)).error ? false :
-      true;
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
     const searchParams = new URLSearchParams(window.location.search);
     const code = searchParams.get("code");
-    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    this.setState({
+      showWelcomeScreen: !(code || isTokenValid)
+    });
     if ((code || isTokenValid) && this.mounted) {
       getEvents().then((events) => {
         if (this.mounted) {
-          this.setState({ events, locations: extractLocations(events) });
+          this.setState({
+            events,
+            locations: extractLocations(events)
+          });
         }
       });
     }
@@ -57,34 +46,78 @@ class App extends Component {
     this.mounted = false;
   }
 
+  updateEvents = (location, eventCount) => {
+    console.log('App', eventCount)
+    if (eventCount === undefined) {
+      eventCount = this.state.numberOfEvents;
+    } else (
+      this.setState({ numberOfEvents: eventCount })
+    )
+    if (location === undefined) {
+      location = this.state.locationSelected;
+    }
+    console.log(eventCount, location)
+    getEvents().then((events) => {
+      let locationEvents = location === "all" ? events : events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents.slice(0, eventCount),
+        numberOfEvents: eventCount,
+        locationSelected: location,
+      });
+    });
+  }
 
-  updateNumberOfEvents = (numberOfEvents) => {
-    this.setState(
-      {
-        numberOfEvents,
-      },
-      this.updateEvents(this.state.locations, numberOfEvents)
-    );
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return { city, number };
+    })
+    return data;
   };
 
-
+  updateNumberofEvents(number) {
+    this.setState({
+      numberOfEvents: number
+    })
+  }
 
   render() {
-
-    // if (this.state.showWelcomeScreen === undefined)
-
+    if (this.state.showWelcomeScreen === undefined) return <div className="App" />
     return (
-      <div className="App" >
 
-        <NumberOfEvents numberOfEvents={NumberOfEvents}
-          updateNumberOfEvents={this.updateNumberOfEvents} />
+      <div className="App">
+
+        <OfflineAlert text={this.state.OfflineAlertText} />
+        <h1>Meet App</h1>
+        <h4>Choose your nearest city</h4>
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
-        <EventList events={this.state.events} />
-        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
-          getAccessToken={() => { getAccessToken() }} />
 
+        <NumberOfEvents updateEvents={(value) => this.updateNumberofEvents(value)} numberOfEvents={this.state.numberOfEvents} />
+
+        <h4>Events in each city</h4>
+
+        <div className='data-vis-wrapper'>
+
+          <EventGenre events={this.state.events} locations={this.state.locations} />
+
+          <ResponsiveContainer height={400} >
+            <ScatterChart margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis type="category" dataKey="city" name="city" />
+              <YAxis type="number" dataKey="number" name="number of events" />
+              <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+              <Scatter data={this.getData()} fill="#8884d8" />
+            </ScatterChart>
+          </ResponsiveContainer>
+
+        </div>
+        <EventList events={this.state.events} />
+        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={() => { getAccessToken() }} />
       </div>
     );
   }
 }
+
 export default App;
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -5,23 +5,23 @@ class NumberOfEvents extends Component {
 
 
     state = {
-        numberOfEvents: 32,
+
         errorText: ''
     }
 
     handleInputChanged = (event) => {
         const value = event.target.value;
+        // debugger
         if (value < 1 || value > 32) {
             this.setState({
-                numberOfEvents: '',
+
                 errorText: 'Please enter a number between 1 and 32',
             })
         } else {
-            this.setState({
-                numberOfEvents: value
-            });
+
+            console.log('number of events', value)
+            this.props.updateNumberofEvents(value);
         }
-        this.props.updateNumberOfEvents(event.target.value);
     };
 
     render() {
@@ -35,7 +35,7 @@ class NumberOfEvents extends Component {
                     type='number'
                     className='inputNumberOfEvents'
                     onChange={this.handleInputChanged}
-                    value={this.state.numberOfEvents}
+                    value={this.props.numberOfEvents}
                 />
             </div>
         )
